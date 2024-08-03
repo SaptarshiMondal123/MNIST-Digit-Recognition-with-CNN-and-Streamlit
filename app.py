2ab88ea6ac5e4afb8aec5e7cfc1e3eaca3fdc219
Saptarshi Mondal <141300787+SaptarshiMondal123@users.noreply.github.com>
2024-08-03 12:57:56 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import tensorflow as tf
 import numpy as np
 from PIL import ImageOps, Image
 
-# Function to load the model with error handling
 def load_model():
     try:
         model = tf.keras.models.load_model('model.keras')
@@ -15,16 +14,14 @@ def load_model():
 
 model = load_model()
 
-# Function to preprocess the image
 def preprocess_image(image):
-    image = ImageOps.grayscale(image)  # Convert to grayscale
-    image = ImageOps.invert(image)  # Invert colors: background should be black
-    image = image.resize((28, 28))  # Resize to 28x28
+    image = ImageOps.grayscale(image)
+    image = ImageOps.invert(image)  
+    image = image.resize((28, 28))  
     image = np.array(image).astype('float32') / 255
-    image = np.expand_dims(image, axis=(0, -1))  # Ensure the image shape is (1, 28, 28, 1)
+    image = np.expand_dims(image, axis=(0, -1))  
     return image
 
-# Function to make predictions
 def recognize(image):
     if image is not None:
         preprocessed_image = preprocess_image(image)
@@ -35,25 +32,22 @@ def recognize(image):
     else:
         return {}
 
-# Streamlit app
 st.title("MNIST Digit Recognizer")
 
 st.write("Draw a digit in the box below:")
 
-# Create a canvas component
 canvas_result = st_canvas(
-    fill_color="white",  # Drawing background color
+    fill_color="white", 
     stroke_width=10,
     stroke_color="black",
     background_color="white",
     update_streamlit=True,
-    height=300,  # Increased height for better drawing
-    width=300,  # Increased width for better drawing
+    height=300,  
+    width=300,  
     drawing_mode="freedraw",
     key="canvas",
 )
 
-# Process the canvas result
 if canvas_result.image_data is not None:
     img = canvas_result.image_data
     img = Image.fromarray((img[:, :, :3] * 255).astype('uint8'), 'RGB')
